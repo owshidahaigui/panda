4a3123f5e6f84ab4a555e29ba7ac5e91304355c5
Tedu <330150956@qq.com>
2019-08-26 20:26:54 +0800

    dsfsadf

--- checklogin_mid.py
+++ checklogin_mid.py
@@ -26,6 +26,7 @@ class VisitLimit(MiddlewareMixin):
         ip_address = request.META['REMOTE_ADDR']  # 得到IP地址
         # if not re.match('^/test', request.path_info):
         #     return
+	#放到redis中去，设置一个5s的有效时间，如果某个值大于一定次数，就把ip放到另一个redis的集合中去，有限时间设置一天，禁止用户登录一天
         times = self.visit_times.get(ip_address, 0)
         print("IP:", ip_address, '已经访问过', times, '次!:', request.path_info)
         self.visit_times[ip_address] = times + 1
